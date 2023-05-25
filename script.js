7d41fe0e6fe91a75110eb555b90786eb239f5cc0
Daniel Hristov <72865668+danihristov06@users.noreply.github.com>
2023-05-25 11:14:59 +0300

    new file:   Accounts.json 	modified:   index.html 	modified:   script.js 	modified:   server.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
 const panel = document.querySelector('#panel');
+let param = window.location.search;
 var slider = document.getElementById("myRange");
 var tooltip = document.getElementsByClassName("tooltip");
+var form = document.getElementsByClassName("form");
+let number = [0, 1, 2, 9];
 
-slider.onmousemove = e => {
-        tooltip[0].style.paddingLeft = `${slider.value*9.4}%`;
-        tooltip[0].innerText = slider.value;
-        tooltip[0].style.opacity = 1;
-    }
-    slider.onmouseleave = e => {
-        tooltip[0].style.opacity = 0;
+const equalsCheck = (a, b) => {
+    return JSON.stringify(a) === JSON.stringify(b);
 }
 
+slider.onmousemove = e => {
+    tooltip[0].style.paddingLeft = `${slider.value * 9.4}%`;
+    tooltip[0].innerText = slider.value;
+    tooltip[0].style.opacity = 1;
+}
+slider.onmouseleave = e => {
+    tooltip[0].style.opacity = 0;
+}
 
-for (let index = 1; index <= 10; index++) {
+if (param.includes("success=true")) {
+    form[0].style.display = 'none';
+    panel.style.display = 'block';
+    console.log(slider.value);
+}
+
+if(param.includes("something=true")){
+    console.log("something")
+}
+
+for (let index = 0; index < 10; index++) {
     const button = document.createElement("div");
-    button.innerText = index;
+    button.innerText = index + 1;
     button.className = "buttons number-" + index;
     panel.appendChild(button);
 }
 const buttons = document.getElementsByClassName("buttons");
 for (let index = 0; index < buttons.length; index++) {
-    const button = document.getElementsByClassName("number-" + (index + 1))[0];
-    buttons[index].onmousedown = function() {
+    const button = document.getElementsByClassName("number-" + (index))[0];
+    buttons[index].onmousedown = function () {
         button.classList.toggle("active");
     }
 }
 
 for (let index = 0; index < buttons.length; index++) {
-    const button = document.getElementsByClassName("number-" + (index + 1))[0];
-    console.log(button.className);
-    if(button.className <= "buttons number-1"){ // if user has access add available tag
-        button.classList.toggle("available");
-    }
+    const button = document.getElementsByClassName("number-" + (index))[0];
+    // console.log(button.classList[1])
+    button.classList.forEach(element => {
+        number.forEach(el => {
+            if (element[7] == el) {
+                button.classList.toggle("available");
+            }
+        })
+    });
 }
--- server.js
+++ server.js
@@ -1,25 +1,84 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
 const port = 3000;
+let param = '';
+let body;
+let database;
+let newRecord;
+// var floors = 1;
 
-const server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    console.log(q.name + ' ' + q.psw + ' ' + q.remember);
-    fs.readFile('index.html', function(error, data){
-        if(error){
-            res.writeHead(404);
-            res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
+function readDataFromFile() {
+    const data = fs.readFileSync("Accounts.json", 'utf8');
+    return JSON.parse(data);
+}
+
+function writeDataToFile() {
+    const data = JSON.stringify(database, null, 2);
+    fs.writeFileSync("Accounts.json", data, 'utf8');
+}
+
+function convertToJSON(input) {
+    const jsonOutput = {};
+
+    for (const item of input) {
+        const [key, value] = item.split('=');
+        jsonOutput[key] = value;
+    }
+
+    return jsonOutput;
+}
+
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if (req.method === 'POST') {
+        req.on('data', chunk => {
+            database = readDataFromFile();
+            body = '';
+            body += chunk.toString();
+            body = body.replace('undefined', "");
+            body = body.split("&");
+            param = '';
+
+            newRecord = convertToJSON(body);
+            // database.forEach(element => {
+            //     if(element != newRecord){
+            //         console.log("creted.");
+            //         database.push(newRecord);
+            //         writeDataToFile();
+            //     } else{
+            //         param = '?success=true';
+            //         console.log("exists");
+            //     }
+            // });
+            let exists = false;
+            for (let i = 0; i < database.length; i++) {
+                if (database[i]['psw'] == newRecord['psw']) {
+                    exists = true;
+                    param = '?success=true';
+                    console.log("exists");
+                    break; // Exit the loop once a match is found
+                }
+            }
+            if (!exists) {
+                console.log("created.");
+                database.push(newRecord);
+                writeDataToFile();
+            }
+
+        });
+        req.on('end', () => {
+            // console.log('POST data:', body);
+            res.writeHead(302, { Location: "http://127.0.0.1:5500/index.html" + param });
+            res.end();
+        })
+    };
+
+
 })
 
-server.listen(port, function(error){
-    if(error){
+server.listen(port, function (error) {
+    if (error) {
         console.log('Something went wrong', error);
     } else {
         console.log('Server is listening on port ' + port);
